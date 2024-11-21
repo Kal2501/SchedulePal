5171a2de96f20ee8e30aae1e54bd5eb04d8fcb64
Kalingga Dwindra Putraka <kalingga221@gmail.com>
2024-11-21 22:08:26 +0800

    responsive n fix

--- edit.php
+++ edit.php
@@ -1,3 +1,36 @@
+<?php
+session_start();
+$user = $_SESSION['user'];
+if (!isset($_SESSION['user'])) {
+    header("Location: login.php");
+    exit();
+}
+
+include 'include/connection.php';
+include 'include/function.php';
+
+$id_acara = $_GET['id'];
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $formData = [
+        // 'judul_acara' => $_POST['judul_acara'],
+        'deskripsi' => $_POST['deskripsi'],
+        'lokasi' => $_POST['lokasi'],
+        'tanggal' => $_POST['tanggal'],
+        'waktu' => $_POST['waktu']
+    ];
+    updateschedule($conn, $id_acara, $formData);
+}
+
+$schedule = ambilidschedule($conn, $id_acara);
+
+
+// if (!$schedule) {
+//     gagalupdate("Schedule tidak ditemukan!");
+// }
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,24 +38,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles/edcre.css">
-    <title>Document</title>
+    <title>Edit Schedule</title>
 </head>
 
 <body>
     <?php
     require 'templates/navbar.php';
     ?>
-    <form action="">
+
+    <form method="POST" action="">
         <h1>Ubah Schedule</h1>
         <h2>Details</h2>
-        <p class="prev-judul">Judul Acara</p>
-        <input type="text" placeholder="Deskripsi Acara" required>
-        <input type="text" placeholder="Lokasi" required>
+        <p class="prev-judul"><?= htmlspecialchars($schedule['judul_acara']); ?></p>
+        <!-- <input type="text" name="judul_acara" value="" required> -->
+
+        <input type="text" name="deskripsi" placeholder="Deskripsi Acara"
+            value="<?= htmlspecialchars($schedule['deskripsi']); ?>" required>
+
+        <input type="text" name="lokasi" placeholder="Lokasi" value="<?= htmlspecialchars($schedule['lokasi']); ?>"
+            required>
+
         <h2>Tanggal dan Waktu</h2>
-        <input type="date" placeholder="Tanggal" required>
-        <input type="time" name="" id="" required>
-        <input type="button" value="Ubah">
+        <input type="date" name="tanggal" value="<?= htmlspecialchars($schedule['tanggal']); ?>" required>
+
+        <input type="time" name="waktu" value="<?= htmlspecialchars($schedule['waktu']); ?>" required>
+
+        <input type="submit" value="Ubah">
     </form>
+
     <?php
     require 'templates/footer.php';
     ?>
--- schedulefak.php
+++ schedulefak.php
@@ -39,9 +39,12 @@ $jadwal = ambilschedule($conn, $fakultas, $limit, $offset);
         <div class="filter">
             <form action="" method="GET">
                 <?php foreach ($daftar_fakultas as $f): ?>
-                    <button type="submit" name="fakultas" value="<?= htmlspecialchars($f['id']); ?>">
-                        <?= htmlspecialchars($f['nama']); ?>
-                    </button>
+                    <div>
+                        <button type="submit" name="fakultas" value="<?= htmlspecialchars($f['id']); ?>">
+                            <img src="icons_fakultas/engineering.png" alt=""
+                                width="25px"><?= htmlspecialchars($f['nama']); ?>
+                        </button>
+                    </div>
                 <?php endforeach; ?>
                 <button type="submit" name="fakultas" value="">Semua Fakultas</button>
             </form>
@@ -51,14 +54,24 @@ $jadwal = ambilschedule($conn, $fakultas, $limit, $offset);
         <?php if ($jadwal->num_rows > 0): ?>
             <?php while ($row = $jadwal->fetch_assoc()): ?>
                 <div class="card">
-                    <div>
+                    <div class="left">
                         <h2><?= htmlspecialchars($row["judul_acara"]); ?></h2>
                         <p><?= htmlspecialchars($row["deskripsi"]); ?></p>
                         <p><?= htmlspecialchars($row["lokasi"]); ?></p>
                     </div>
-                    <div>
-                        <p><?= htmlspecialchars(date("m/d/Y", strtotime($row["tanggal"]))); ?></p>
-                        <p><?= htmlspecialchars(date("H:i", strtotime($row["waktu"]))); ?></p>
+                    <div class="right">
+                        <p><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
+                                fill="000000">
+                                <path
+                                    d="M200-80q-33 0-56.5-23.5T120-160v-560q0-33 23.5-56.5T200-800h40v-80h80v80h320v-80h80v80h40q33 0 56.5 23.5T840-720v560q0 33-23.5 56.5T760-80H200Zm0-80h560v-400H200v400Zm0-480h560v-80H200v80Zm0 0v-80 80Zm280 240q-17 0-28.5-11.5T440-440q0-17 11.5-28.5T480-480q17 0 28.5 11.5T520-440q0 17-11.5 28.5T480-400Zm-160 0q-17 0-28.5-11.5T280-440q0-17 11.5-28.5T320-480q17 0 28.5 11.5T360-440q0 17-11.5 28.5T320-400Zm320 0q-17 0-28.5-11.5T600-440q0-17 11.5-28.5T640-480q17 0 28.5 11.5T680-440q0 17-11.5 28.5T640-400ZM480-240q-17 0-28.5-11.5T440-280q0-17 11.5-28.5T480-320q17 0 28.5 11.5T520-280q0 17-11.5 28.5T480-240Zm-160 0q-17 0-28.5-11.5T280-280q0-17 11.5-28.5T320-320q17 0 28.5 11.5T360-280q0 17-11.5 28.5T320-240Zm320 0q-17 0-28.5-11.5T600-280q0-17 11.5-28.5T640-320q17 0 28.5 11.5T680-280q0 17-11.5 28.5T640-240Z" />
+                            </svg>
+                            <?= htmlspecialchars(date("m/d/Y", strtotime($row["tanggal"]))); ?>
+                        </p>
+                        <p> <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
+                                fill="00000">
+                                <path
+                                    d="m612-292 56-56-148-148v-184h-80v216l172 172ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-400Zm0 320q133 0 226.5-93.5T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 133 93.5 226.5T480-160Z" />
+                            </svg><?= htmlspecialchars(date("H:i", strtotime($row["waktu"]))); ?></p>
                     </div>
                 </div>
             <?php endwhile; ?>
