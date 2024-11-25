efaf803af84f0f775915497a439c8efb64a29467
Ibnuansari <161904852+Ibnuansari@users.noreply.github.com>
2024-11-25 20:53:11 +0800

    profile.php
    update profil dihapus dari database

--- profile.php
+++ profile.php
@@ -20,12 +20,13 @@ $profile = getUserProfile($conn, $NIM);
 
 if (isset($_FILES['profile-pic'])) {
     $result = updateProfilePicture($conn, $NIM, $_FILES['profile-pic']);
-    echo "<script>alert('" . $result['message'] . "')</script>";
+    echo "<script>alert('" . $result['message'] . "');</script>";
+    $profile = getUserProfile($conn, $NIM);
 }
 
 if (isset($_GET['delete']) && isset($_GET['id'])) {
     $result = deleteSchedule($conn, $_GET['id'], $NIM);
-    echo "<script>alert('" . $result['message'] . "')</script>";
+    echo "<script>alert('" . $result['message'] . "');</script>";
 }
 
 if (isset($user['foto'])) {
@@ -47,9 +48,7 @@ if (isset($user['foto'])) {
 </head>
 
 <body>
-    <?php
-    require 'templates/navbar.php';
-    ?>
+    <?php require 'templates/navbar.php'; ?>
     <div class="content">
 
         <h1>Profile</h1>
@@ -86,18 +85,14 @@ if (isset($user['foto'])) {
                     <div class="right">
                         <div class="date-times">
                             <p>
-                                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
-                                    fill="000000">
-                                    <path
-                                        d="M200-80q-33 0-56.5-23.5T120-160v-560q0-33 23.5-56.5T200-800h40v-80h80v80h320v-80h80v80h40q33 0 56.5 23.5T840-720v560q0 33-23.5 56.5T760-80H200Zm0-80h560v-400H200v400Zm0-480h560v-80H200v80Zm0 0v-80 80Zm280 240q-17 0-28.5-11.5T440-440q0-17 11.5-28.5T480-480q17 0 28.5 11.5T520-440q0 17-11.5 28.5T480-400Zm-160 0q-17 0-28.5-11.5T280-440q0-17 11.5-28.5T320-480q17 0 28.5 11.5T360-440q0 17-11.5 28.5T320-400Zm320 0q-17 0-28.5-11.5T600-440q0-17 11.5-28.5T640-480q17 0 28.5 11.5T680-440q0 17-11.5 28.5T640-400ZM480-240q-17 0-28.5-11.5T440-280q0-17 11.5-28.5T480-320q17 0 28.5 11.5T520-280q0 17-11.5 28.5T480-240Zm-160 0q-17 0-28.5-11.5T280-280q0-17 11.5-28.5T320-320q17 0 28.5 11.5T360-280q0 17-11.5 28.5T320-240Zm320 0q-17 0-28.5-11.5T600-280q0-17 11.5-28.5T640-320q17 0 28.5 11.5T680-280q0 17-11.5 28.5T640-240Z" />
+                                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="000000">
+                                    <path d="M200-80q-33 0-56.5-23.5T120-160v-560q0-33 23.5-56.5T200-800h40v-80h80v80h320v-80h80v80h40q33 0 56.5 23.5T840-720v560q0 33-23.5 56.5T760-80H200Zm0-80h560v-400H200v400Zm0-480h560v-80H200v80Zm0 0v-80 80Zm280 240q-17 0-28.5-11.5T440-440q0-17 11.5-28.5T480-480q17 0 28.5 11.5T520-440q0 17-11.5 28.5T480-400Zm-160 0q-17 0-28.5-11.5T280-440q0-17 11.5-28.5T320-480q17 0 28.5 11.5T360-440q0 17-11.5 28.5T320-400Zm320 0q-17 0-28.5-11.5T600-440q0-17 11.5-28.5T640-480q17 0 28.5 11.5T680-440q0 17-11.5 28.5T640-400ZM480-240q-17 0-28.5-11.5T440-280q0-17 11.5-28.5T480-320q17 0 28.5 11.5T520-280q0 17-11.5 28.5T480-240Zm-160 0q-17 0-28.5-11.5T280-280q0-17 11.5-28.5T320-320q17 0 28.5 11.5T360-280q0 17-11.5 28.5T320-240Zm320 0q-17 0-28.5-11.5T600-280q0-17 11.5-28.5T640-320q17 0 28.5 11.5T680-280q0 17-11.5 28.5T640-240Z" />
                                 </svg>
                                 <?php echo htmlspecialchars(date('d-m-Y', strtotime($schedule['tanggal']))); ?>
                             </p>
                             <p>
-                                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
-                                    fill="00000">
-                                    <path
-                                        d="m612-292 56-56-148-148v-184h-80v216l172 172ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-400Zm0 320q133 0 226.5-93.5T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 133 93.5 226.5T480-160Z" />
+                                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="00000">
+                                    <path d="m612-292 56-56-148-148v-184h-80v216l172 172ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-400Zm0 320q133 0 226.5-93.5T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 133 93.5 226.5T480-160Z" />
                                 </svg>
                                 <?php echo htmlspecialchars(date('H:i', strtotime($schedule['waktu']))); ?>
                             </p>
@@ -119,9 +114,6 @@ if (isset($user['foto'])) {
             </div>
         </div>
     </div>
-    <?php
-    require 'templates/footer.php';
-    ?>
+    <?php require 'templates/footer.php'; ?>
 </body>
-
 </html>
